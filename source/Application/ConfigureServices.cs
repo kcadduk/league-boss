d2492dc6f56859568ea53a43989ada740c3f7f40
Kevin Cadd <kevin.cadd@hotmail.co.uk>
2024-06-20 23:05:04 +0100

    feat: Add FastEndpoints, Serilog and update application configuration
    Added FastEndpoints and FastEndpoints.Swagger packages to the application. Also integrated Serilog for logging. Substantially updated Program.cs and created a new ConfigureServices.cs file to handle application services configuration. Adjusted the launchSettings.json to disable automatic browser launch.

---
+++ source/Application/ConfigureServices.cs
@@ -0,0 +1,36 @@
+namespace LeagueBoss.Application;
+
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Serilog;
+
+public static class ConfigureServices
+{
+    internal static IServiceCollection ConfigureWebApplicationServices(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.ConfigureFastEndpointsWithAuth()
+            .AddCors()
+            .AddSerilog((sp, serilog) =>
+            {
+                serilog
+                    .WriteTo.Console()
+                    .ReadFrom.Services(sp);
+            });;
+        return serviceCollection;
+    }
+
+    private static IServiceCollection ConfigureFastEndpointsWithAuth(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddFastEndpoints()
+            .SwaggerDocument();
+        
+        serviceCollection
+            .AddAuthentication(auth => { auth.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme; })
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        serviceCollection.AddAuthorization();
+        
+        return serviceCollection;
+    }
+}
