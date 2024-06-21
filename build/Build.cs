256b025a3810ae7cec157c3861a29c66649a3dd6
Kevin Cadd <kevin.cadd@hotmail.co.uk>
2024-06-21 17:55:26 +0100

    feat: Refactor build process and add testing
    The main function has been updated to trigger testing instead of compilation. Additional libraries, Nuke.Common.Tools.DotNet, Nuke.Common.Tools.NerdbankGitVersioning, and Serilog have been imported for usage in the build process. This change also includes creating/cleaning a temporary directory, printing the version information, and running dotnet restore, build, and test commands.

--- build/Build.cs
+++ build/Build.cs
@@ -6,7 +6,10 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.NerdbankGitVersioning;
 using Nuke.Common.Utilities.Collections;
+using Serilog;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
@@ -19,26 +22,49 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.Test);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
-
+    
+    [NerdbankGitVersioning]
+    readonly NerdbankGitVersioning NerdbankVersioning;
+    
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            TemporaryDirectory.CreateOrCleanDirectory();
+        });
+    
+    Target Print => _ => _
+        .TriggeredBy(Compile)
+        .Executes(() =>
+        {
+            Log.Information("NerdbankVersioning = {Value}", NerdbankVersioning.SimpleVersion);
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetTasks.DotNetRestore();
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetTasks.DotNetBuild(s => 
+                s.SetNoRestore(true));
         });
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetTest();
+        });
+    
+    
 
 }
