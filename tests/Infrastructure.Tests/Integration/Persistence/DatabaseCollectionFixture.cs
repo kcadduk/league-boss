e6f2883965a77be99ccdac136d9974dc127dd9e6
Kevin Cadd <kevin.cadd@hotmail.co.uk>
2024-06-22 11:23:02 +0100

    feat: Add Infrastructure.Tests project and DatabaseFixture
    A new project Infrastructure.Tests has been added to the solution to incorporate testing for the infrastructure layer. Along with this, a DatabaseFixture class has been added to the project for test setup and teardown, handling database migrations and connections for testing.

---
+++ tests/Infrastructure.Tests/Integration/Persistence/DatabaseCollectionFixture.cs
@@ -0,0 +1,59 @@
+namespace LeagueBoss.Infrastructure.Tests.Integration.Persistence;
+
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Testcontainers.MsSql;
+
+public class DatabaseFixture : IAsyncLifetime
+{
+    private readonly MsSqlContainer _sqlContainer;
+    public ServiceProvider Services { get; } = null!;
+
+    public string ConnectionString { get; private set; } = null!;
+
+    public DatabaseFixture()
+    {
+        _sqlContainer = new MsSqlBuilder()
+            .WithPassword("password@123")
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _sqlContainer.StartAsync();
+        BuildServiceProvider();
+
+        var databaseMigrator = Services.GetRequiredService<IDatabaseMigrationHandler>();
+        await databaseMigrator.Apply();
+    }
+
+    private void BuildServiceProvider()
+    {
+        var serviceCollection = new ServiceCollection()
+            .ConfigureInfrastructureServices();
+
+        serviceCollection.RemoveAll(typeof(DbContextOptions<UsersDbContext>));
+        serviceCollection.AddSingleton<DbContextOptions<UsersDbContext>>(_ =>
+            new DbContextOptionsBuilder<UsersDbContext>()
+                .UseSqlServer(ConnectionString)
+                #if DEBUG
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                #endif
+                .Options);
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _sqlContainer.DisposeAsync();
+    }
+}
+
+[CollectionDefinition(nameof(DatabaseFixture))]
+public class DatabaseCollectionFixture : ICollectionFixture<DatabaseFixture>
+{
+}
