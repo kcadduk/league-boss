57473ebd03c45e3b497468294c091a602a4bb303
Kevin Cadd <kevin.cadd@hotmail.co.uk>
2024-06-22 13:05:29 +0100

    feat: Expand IDbContext interface
    The IDbContext interface has been extended to include methods for adding, updating, attaching, and removing entities, among others. This enhances the functionality of the interface, allowing it to handle more tasks related to database management. All important methods from DbContext class of Entity Framework are now included in our IDbContext interface.

--- source/Application/DbContext/IDbContext.cs
+++ source/Application/DbContext/IDbContext.cs
@@ -1,6 +1,34 @@
 namespace LeagueBoss.Application.DbContext;
 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
 public interface IDbContext
 {
-    
+    DbSet<TEntity> Set<TEntity>() where TEntity : class;
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    EntityEntry Add(object entity);
+    EntityEntry<TEntity> Add<TEntity>(TEntity entity) where TEntity : class;
+
+    ValueTask<EntityEntry> AddAsync(object entity, CancellationToken cancellationToken = default);
+    ValueTask<EntityEntry<TEntity>> AddAsync<TEntity>(TEntity entity, CancellationToken cancellationToken = default)
+        where TEntity : class;
+
+    void AddRange(params object[] entities);
+    Task AddRangeAsync(params object[] entities);
+    Task AddRangeAsync(IEnumerable<object> entities, CancellationToken cancellationToken = default);
+
+    EntityEntry Update(object entity);
+    EntityEntry<TEntity> Update<TEntity>(TEntity entity) where TEntity : class;
+
+    EntityEntry Attach(object entity);
+    EntityEntry<TEntity> Attach<TEntity>(TEntity entity) where TEntity : class;
+
+    EntityEntry Remove(object entity);
+    EntityEntry<TEntity> Remove<TEntity>(TEntity entity)
+        where TEntity : class;
+    ChangeTracker ChangeTracker { get; }
+    DatabaseFacade Database { get; }
 }
