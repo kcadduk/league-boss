c10e7861fc237a08d0a97162d2a647b70980fa5b
Kevin Cadd <kevin.cadd@hotmail.co.uk>
2024-10-12 16:24:15 +0100

    feat: Add Node.js integration and NPM package restore.
    Included `Aspire.Hosting.NodeJs` package and added a new NPM application. Configured the build process to restore NPM packages if `node_modules` is missing and updated `Program.cs` to handle the new Node.js app with specified environment and endpoints.

--- source/ApplicationHost/Program.cs
+++ source/ApplicationHost/Program.cs
@@ -1,5 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Api>("api");
+var api = builder.AddProject<Projects.Api>("api");
+
+var node = builder.AddNpmApp("react", "../web-ui")
+    .WithReference(api)
+    .WithEnvironment("BROWSER", "none")
+    .WithHttpEndpoint(port: 5173, env: "VITE_PORT", targetPort: 5174)
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
