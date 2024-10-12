bf3c951f9ad6780660e199f601ebbb353729c29d
Kevin Cadd <kevin.cadd@hotmail.co.uk>
2024-10-12 16:25:11 +0100

    feat: Add Docker configuration and update dependencies
    This commit introduces a Dockerfile and nginx configuration template for containerizing the web UI. It also updates various dependencies in the package.json and adjusts the vite configuration to handle server and build settings.

--- source/web-ui/vite.config.ts
+++ source/web-ui/vite.config.ts
@@ -1,14 +1,32 @@
-import { defineConfig } from 'vite'
-import path from "path"
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import {TanStackRouterVite} from '@tanstack/router-vite-plugin'
+import {defineConfig, loadEnv} from 'vite';
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [TanStackRouterVite({}), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  }
+/** @type {import('vite').UserConfig} */
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [viteReact(), TanStackRouterVite()],
+        server: {
+            port: parseInt(env.VITE_PORT),
+            proxy: {
+                '/api': {
+                    target: process.env.services__api__https__0 ||
+                        process.env.services__api__http__0,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                    secure: false,
+                }
+            }
+        },
+        build: {
+            outDir: 'dist',
+            rollupOptions: {
+                input: './index.html'
+            }
+        }
+    }
 })
